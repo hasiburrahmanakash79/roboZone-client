e00dca14b10ca139032fe21c4078c1aa290dd146
hasiburrahmanakash79 <hasiburrahmanakash79@gmail.com>
2023-05-21 13:14:08 +0600

    implement search system in all toy section

--- src/AllToys/AllToys.jsx
+++ src/AllToys/AllToys.jsx
@@ -1,10 +1,14 @@
 import { useEffect, useState } from "react";
 import useTitle from "../Hooks/useTitle";
 import SingleToy from "./SingleToy";
+import { FaSearch } from "react-icons/fa";
 
 const AllToys = () => {
   useTitle("AllToys");
+
   const [toys, setToys] = useState([]);
+  const [search, setSearch] = useState("");
+  const [searchData, setSearchData] = useState([]);
 
   const url = "https://robot-world-server.vercel.app/allToys";
   useEffect(() => {
@@ -14,11 +18,37 @@ const AllToys = () => {
         setToys(data);
       });
   }, []);
+
+  useEffect(() => {
+    if (search?.length > 0) {
+      const filterResult = toys.filter((toy) =>
+        toy?.toyName?.toLowerCase().includes(search?.toLowerCase())
+      );
+      setSearchData(filterResult);
+    } else {
+      setSearchData(toys);
+    }
+  }, [search, toys]);
+
+  console.log(searchData);
   return (
     <div>
-      <h1 className="text-4xl font-semibold text-center my-7">
+      <h1 className="text-4xl text-center font-semibold my-7">
         Total Toys in Our Shop
       </h1>
+      <div className="form-control my-5">
+        <div className="input-group">
+          <input
+            onChange={(event) => setSearch(event.target.value)}
+            type="text"
+            placeholder="Search your toy"
+            className="input input-bordered"
+          />
+          <button className="btn btn-square">
+            <FaSearch></FaSearch>
+          </button>
+        </div>
+      </div>
       <div>
         <div className="overflow-x-auto">
           <table className="table table-compact w-full">
@@ -34,7 +64,7 @@ const AllToys = () => {
               </tr>
             </thead>
             <tbody>
-              {toys.map((toy) => (
+              {searchData.map((toy) => (
                 <SingleToy key={toy._id} toy={toy}></SingleToy>
               ))}
             </tbody>
--- src/Home/Category/Category.jsx
+++ src/Home/Category/Category.jsx
@@ -1,7 +1,41 @@
+import { useEffect, useState } from "react";
 import { Tab, TabList, TabPanel, Tabs } from "react-tabs";
 import "react-tabs/style/react-tabs.css";
 
 const Category = () => {
+  const [allData, setAllData] = useState([]);
+
+  const [industrial, setIndustrial] = useState(true);
+  const [remote, setRemote] = useState(false);
+  const [ai, setAi] = useState(false);
+  const [car, setCar] = useState(false);
+
+  const url = "https://robot-world-server.vercel.app/allToys";
+  useEffect(() => {
+    fetch(url)
+      .then((res) => res.json())
+      .then((data) => {
+        setAllData(data);
+      });
+  }, []);
+
+  console.log(allData);
+
+  const filterData = allData.filter((item) => {
+    if (industrial) {
+      return item.category === "Industrial";
+    }
+    if (remote) {
+      return item.category === "Remote control";
+    }
+    if (ai) {
+      return item.category === "AI Robot";
+    }
+    if (car) {
+      return item.category === "Robotic Car";
+    }
+  });
+  console.log(filterData);
   return (
     <div className="my-8 px-2">
       <div
@@ -10,7 +44,10 @@ const Category = () => {
         data-aos-offset="300"
         data-aos-easing="ease-in-sine"
       >
-        <h1 className="text-2xl md:text-4xl my-2 font-semibold">Category of Items</h1>
+        <h1 className="text-2xl md:text-4xl my-2 font-semibold">
+          Category of Items
+          
+        </h1>
         <p>
           Here is an overview of some types of <br /> robots that every
           manufacturer should know.
@@ -18,10 +55,30 @@ const Category = () => {
       </div>
       <Tabs>
         <TabList>
-          <Tab>Industrial</Tab>
-          <Tab>Remote Control</Tab>
-          <Tab>AI Robot</Tab>
-          <Tab>Robotic Car</Tab>
+          <Tab onClick={() => {
+            setIndustrial(true)
+            setRemote(false)
+            setCar(false)
+            setAi(false)
+          }}>Industrial</Tab>
+          <Tab onClick={() => {
+            setIndustrial(false)
+            setRemote(true)
+            setCar(false)
+            setAi(false)
+          }}>Remote Control</Tab>
+          <Tab onClick={() => {
+            setIndustrial(false)
+            setRemote(false)
+            setCar(false)
+            setAi(true)
+          }}>AI Robot</Tab>
+          <Tab onClick={() => {
+            setIndustrial(false)
+            setRemote(false)
+            setCar(true)
+            setAi(false)
+          }}>Robotic Car</Tab>
         </TabList>
 
         <TabPanel></TabPanel>
